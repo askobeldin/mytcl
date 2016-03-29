b16216ee2fa94b5bd083b7aae064eab9cdb0c0c8
askobeldin <askobeldin@gmail.com>
2016-03-29 17:20:23 +0300

    gssd

---
+++ experiments/regsub1.tcl
@@ -0,0 +1,20 @@
+# regsub experiments
+#
+#
+set file tclIO.c
+set r1 {([^\.]*)\.c$}
+
+# regsub {([^\.]*)\.c$} $file {cc -c & -o \1.o} ccCmd
+regsub $r1 $file {cc -c & -o \1.o} ccCmd
+puts $ccCmd
+
+# do nothing
+regsub $r1 test.html {cc -c & -o \1.o} ccCmd
+puts $ccCmd
+
+
+set string "The regular      expression metacharacters       in \$unique1 and \$unique need 
+to be quoted so they are not     treated as metacharacters."
+
+regsub -all {\s+} $string "+" string
+puts $string
